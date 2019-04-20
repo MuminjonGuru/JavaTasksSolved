4662b3dd432e2f50d9993c020131d972224206ea
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-04-20 16:15:43 +0500

    Create ProductFixtures.java

---
+++ java_collections_list_arraylist/ProductFixtures.java
@@ -0,0 +1,7 @@
+public class ProductFixtures {
+
+	Product door = new Product("Wooden Door", 35);
+	Product floorPanel = new Product("Floor Panel", 25);
+	Product window = new Product("Glass Window", 10);
+	
+}
