bad5d700f17d37e54e3251c999e122c7642f8cc4
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-05-12 15:03:04 +0500

    permutations of an given integer array

---
+++ AllPermutationsOfAnInteger.java
@@ -0,0 +1,39 @@
+public class AllPermutationsOfAnInteger {
+
+	public static void printArray(int[] arr, int count) {
+		System.out.println("\n Values stored in array are: ");
+		for (int i=0; i < count; i++) {
+			System.out.println("" + arr[i]);
+		}
+	}
+	
+	public static void swap(int[] arr, int x, int y) {
+		int temp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = temp;
+	}
+	
+	public static void permutation(int[] arr, int i, int length) {
+		if(length == i) {
+			printArray(arr, length);
+			return;
+		}
+		int j = i;
+		for(j=i; j < length; j++) {
+			swap(arr, i, j);
+			permutation(arr, i+1, length);
+			swap(arr, i, j);
+		}
+		return;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		for(int i = 0; i < 5; i++) {
+			arr[i] = i;
+		}
+		permutation(arr, 0, 5);
+
+	}
+
+}
