2fc9e8cb6db2059a12cf2611d58727dbc0eaf173
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-08-06 21:38:47 +0500

    SortString

---
+++ SortString.java
@@ -0,0 +1,41 @@
+class SortString {
+
+    static String arr[] = {
+      "Now", "is", "the", "time", "for", "all", "good", "men",
+      "to", "come", "to", "the", "aid", "of", "their", "country"
+    };
+
+  public static void main(String[] args) {
+    for(int j = 0; j < arr.length; j++) {
+      for(int i = j + 1; i < arr.length; i++) {
+        if(arr[i].compareTo(arr[j]) < 0) {
+          String t = arr[j];
+          arr[j] = arr[i];
+          arr[i] = t;
+        }
+      }
+      System.out.println(arr[j]); 
+    }
+  }
+}
+
+/*
+  Output:
+
+  Now
+  aid
+  all
+  come
+  country
+  for
+  good
+  is
+  men
+  of
+  the
+  the
+  their
+  time
+  to
+  to
+*/
