6771ce73b57e06f2e4f5038121c5d5efc23467fb
Abduraimov Muminjon <muminjonguru@outlook.com>
2019-09-04 14:16:09 +0500

    Create RecTest.java

---
+++ PrintArrayRecursively/RecTest.java
@@ -0,0 +1,18 @@
+package com.company;
+
+class RecTest {
+    int values[];
+
+    RecTest(int i) {
+        values = new int[i];
+    }
+
+    // display array recursively
+    void printArray(int i) {
+        if(i==0) return;
+        else printArray(i-1);
+        System.out.println("[" + (i-1) + "] " + values[i-1]);
+    }
+
+
+}
