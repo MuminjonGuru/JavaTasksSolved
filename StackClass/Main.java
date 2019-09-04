2aa20aff8bf013f60edc98a82f38444b0751a612
Abduraimov Muminjon <muminjonguru@outlook.com>
2019-09-04 07:31:10 +0500

    Create Main.java

---
+++ StackClass/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack myStack1 = new Stack();
+        Stack myStack2 = new Stack();
+
+        // push some numbers onto the stack
+        for(int i=0; i<10; i++) myStack1.push(i);
+        for(int i=10; i<20; i++) myStack2.push(i);
+
+        // pop those numbers off the stack
+        System.out.println("Stack in myStack1: ");
+        for (int i = 0; i<10; i++)
+            System.out.println(myStack1.pop());
+
+        // pop those numbers off the stack
+        System.out.println("Stack in myStack2: ");
+        for (int i = 0; i<10; i++)
+            System.out.println(myStack2.pop());
+    }
+}
